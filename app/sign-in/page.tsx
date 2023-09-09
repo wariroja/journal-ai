8836b24a9bce1c5f5c8e22483da8edd2f8c546e9
Warinya/วริญญา <wariroja@gmail.com>
2023-09-08 22:48:08 -0700

    add utils ai for analysis

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+  <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+    <div>
+      <h1>title</h1>
+      <p>description</p>
+      <div>
+        <button>
+          get started
+        </button>
+      </div>
+    </div>
+  </div>
+  )
+}

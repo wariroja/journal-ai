078456c2fe65901c392b1dc9dbdbefa9c72503c1
Warinya/วริญญา <wariroja@gmail.com>
2023-09-12 23:29:58 -0700

    feat: add dashboard and journal layout and page

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,5 +1,46 @@
-const JournalPage = () => {
-  return <div>Journal</div>
+import NewEntryCard from '@/components/ NewEntryCard'
+import EntryCard from '@/components/EntryCard'
+import { qa } from '@/utils/ai'
+import { getUserFromClerkID } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+import Link from 'next/link'
+
+const getEntries = async () => {
+  const user = await getUserFromClerkID()
+  const data = await prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+    include: {
+      analysis: true,
+    },
+  })
+
+  return data
+}
+
+const JournalPage = async () => {
+  const data = await getEntries()
+  return (
+    <div className="px-6 py-8 bg-zinc-100/50 h-full">
+      <h1 className="text-4xl mb-12">Journals</h1>
+      <div className="my-8">
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        <NewEntryCard />
+        {data.map((entry) => (
+          <div key={entry.id}>
+            <Link href={`/journal/${entry.id}`}>
+              <EntryCard entry={entry} />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default JournalPage

ed51618e6d675893a7553b2aa31f2447fc2b0f3f
Warinya/วริญญา <wariroja@gmail.com>
2023-09-16 14:47:41 -0700

    feat: add chart for sentiment analysis

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -1,18 +1,31 @@
+import HistoryChart from "@/app/components/HistoryChart"
 import { getUserFromClerkID } from "@/utils/auth"
 import { prisma } from "@/utils/db"
 
 
-// const getData = async () => {
-//   const user = await getUserFromClerkID()
-//   const analyses = await prisma.analysis.findMany({
-//     where: {
-      
-//     }
-//   })
-// }
 
-const History = () => {
-  return <div>History</div>
+const getData = async () => {
+  const user = await getUserFromClerkID()
+  const analyses = await prisma.analysis.findMany({
+    where: {
+      userId: user.id
+    },
+  })
+  const sum = analyses.reduce((all, current) => all + current.sentimentScore, 0)
+  const avg = Math.round(sum / analyses.length)
+  return { analyses, avg}
+}
+
+const History = async () => {
+  const { avg, analyses } = await getData()
+  return (
+    <div className='w-full h-full'>
+      <div>{`Ave: ${avg}`}</div>
+      <div className= 'w-full h-full'>
+        <HistoryChart data={analyses} />
+      </div>
+    </div>
+  )
 }
 
 export default History

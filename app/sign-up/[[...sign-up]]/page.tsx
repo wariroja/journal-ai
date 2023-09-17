086ffc9d3f0add79e124ebb849ec4a916e7c1afa
Warinya/วริญญา <wariroja@gmail.com>
2023-09-17 01:15:59 -0700

    fix: remove props signup

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -2,11 +2,6 @@ import { SignUp } from '@clerk/nextjs'
 
 export default function SignUpPage() {
   return (
-    <SignUp
-      path="/sign-up"
-      routing="path"
-      signInUrl="/sign-in"
-      afterSignUpUrl="/journal"
-    />
+    <SignUp />
   )
 }

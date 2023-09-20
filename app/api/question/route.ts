c3087b9b6b61f15c17c58f00f67e3860c9fdefe4
Warinya/วริญญา <wariroja@gmail.com>
2023-09-19 23:04:09 -0700

    fix: even more logs

--- app/api/question/route.ts
+++ app/api/question/route.ts
@@ -4,8 +4,10 @@ import { prisma } from '@/utils/db'
 import { NextResponse } from 'next/server'
 
 export const POST = async (request) => {
+  console.log(request.json())
   const { question } = await request.json()
   const user = await getUserFromClerkID()
+  console.log(user, request)
   const entries = await prisma.journalEntry.findMany({
     where: {
       userId: user.id,
